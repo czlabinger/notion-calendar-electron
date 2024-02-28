a9a5668d9336a6410abb3ef1d5cc3e35cbd05bc6
czlabinger <czlabinger@student.tgm.ac.at>
2024-02-28 16:23:48 +0100

    Notification tests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain} = require('electron');
+const { Notification } = require('electron')
 
 app.on('ready', () => {
 
@@ -10,4 +11,15 @@ app.on('ready', () => {
   // Load the external website
   win.loadURL('https://calendar.notion.so/');
 
+  win.webContents.openDevTools();
+  
+  Notification.requestPermission = () => {};
+  Notification.permission = "granted";
+  win.Notification = function (title, options) {
+    console.log('Notification:', title, options);
+    // Send the notification data to the main process
+    require('electron').ipcRenderer.send('notification', { title, options });
+  };
+
 });
+
