1b3609e5fc7a8ccf4c112e0a259890c10fa85406
czlabinger <118806077+czlabinger@users.noreply.github.com>
2024-02-29 10:28:06 +0100

    Dev (#1)
    * Notification tests
    
    * Opens external links in browser
    
    * fixed formatting
    
    * Bumped pkg ver
    
    * Removed not working code

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 #!/usr/bin/env node
 
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow,ipcMain} = require('electron');
 
 app.on('ready', () => {
 
-  // Create a new window
-  let win = new BrowserWindow({ width: 800, height: 600 });
+    // Create a new window
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
 
-  // Load the external website
-  win.loadURL('https://calendar.notion.so/');
+    // Load the external website
+    win.loadURL('https://calendar.notion.so/');
+
+    //win.webContents.openDevTools();
+
+    win.webContents.setWindowOpenHandler((details) => {
+        require('electron').shell.openExternal(details.url);
+        return {
+            action: 'deny'
+        };
+    });
 
 });
