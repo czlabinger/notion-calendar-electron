6d80b3287dc5293ed1cb52ae947492bd99e3ed4a
czlabinger <czlabinger@student.tgm.ac.at>
2024-02-29 10:16:20 +0100

    fixed formatting

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 #!/usr/bin/env node
 
-const { app, BrowserWindow, ipcMain} = require('electron');
+const { app, BrowserWindow,ipcMain} = require('electron');
 const { Notification } = require('electron')
 
 app.on('ready', () => {
 
-  // Create a new window
-  let win = new BrowserWindow({ width: 800, height: 600 });
+    // Create a new window
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
 
-  // Load the external website
-  win.loadURL('https://calendar.notion.so/');
+    // Load the external website
+    win.loadURL('https://calendar.notion.so/');
 
-  win.webContents.openDevTools();
-  
-  Notification.requestPermission = () => {};
-  Notification.permission = "granted";
-  win.Notification = function (title, options) {
-    console.log('Notification:', title, options);
-    // Send the notification data to the main process
-    require('electron').ipcRenderer.send('notification', { title, options });
-  };
+    win.webContents.openDevTools();
+
+    Notification.requestPermission = () => {};
+    Notification.permission = "granted";
+    win.Notification = function(title, options) {
+        console.log('Notification:', title, options);
+        // Send the notification data to the main process
+        require('electron').ipcRenderer.send('notification', {
+            title,
+            options
+        });
+    };
 
     win.webContents.setWindowOpenHandler((details) => {
-    require('electron').shell.openExternal(details.url);
-    return { action: 'deny' };
+        require('electron').shell.openExternal(details.url);
+        return {
+            action: 'deny'
+        };
     });
-    
-});
 
+});
