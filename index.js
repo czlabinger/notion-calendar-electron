b949f5f852f1fdc0624fa1dfcf66f30ccfc0f85a
czlabinger <czlabinger@student.tgm.ac.at>
2024-03-17 19:30:27 +0100

    added excludedLinks

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
-const { app, BrowserWindow,ipcMain} = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 app.on('ready', () => {
-
     // Create a new window
     let win = new BrowserWindow({
         width: 800,
@@ -13,13 +12,23 @@ app.on('ready', () => {
     // Load the external website
     win.loadURL('https://calendar.notion.so/');
 
-    //win.webContents.openDevTools();
+    // Exclude specific links from being opened in the default browser
+    const excludedLinks = ['https://calendar-api.notion.so/v1/auth'];
 
     win.webContents.setWindowOpenHandler((details) => {
-        require('electron').shell.openExternal(details.url);
-        return {
-            action: 'deny'
-        };
+        // Check if the link is in the excluded list
+        if (excludedLinks.includes(details.url)) {
+            // Handle the link as needed, e.g., open it in the Electron app or ignore it
+            console.log('Excluded link:', details.url);
+            return {
+                action: 'deny'
+            };
+        } else {
+            // Open other links in the default browser
+            require('electron').shell.openExternal(details.url);
+            return {
+                action: 'deny'
+            };
+        }
     });
-
 });
