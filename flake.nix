e9f9c738cc88e05800646b7ea2dc67e0ce1ee3a5
czlabinger <118806077+czlabinger@users.noreply.github.com>
2024-10-31 22:40:21 +0100

    Fix for Nix (#9)
    * Added node2nix
    
    * node2nix
    
    * Fixed for nix

--- flake.nix
+++ flake.nix
@@ -1,59 +1,48 @@
 {
   description = "Electron-based Notion Calendar client";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      version = "1.0.4";
-    in
-    {
-      pkgs.${system}.default = pkgs.stdenv.mkDerivation {
-        pname = "notion-calendar-electron";
-        version = "${version}";
-
-        src = pkgs.fetchFromGitHub {
-          owner = "czlabinger";
-          repo = "notion-calendar-electron";
-          rev = "v${version}";
-          sha256 = "sha256-1jd1dphj6h8sm14c31rbml73bxrcf9gfgg0r0wa27jlmgp3mhkl7"; # Replace with real sha256
+      let
+        pkgs = import nixpkgs { inherit system; };
+        node-modules = pkgs.mkYarnPackage {
+          name = "node-modules";
+          src = ./.;
         };
+        package = pkgs.stdenv.mkDerivation {
+          name = "notion-calendar-electron";
+          src = ./.;
+          buildInputs = [pkgs.yarn node-modules];
 
-        buildInputs = [ pkgs.nodejs pkgs.npm pkgs.git ];
+          buildPhase = ''
+            ${pkgs.yarn}/bin/yarn build
+          '';
 
-        nativeBuildInputs = [ pkgs.electron ];
+          installPhase =  ''
+          mkdir $out
 
-        unpackPhase = ''
-          mkdir -p $sourceRoot
-          tar -xf $src --strip-components=1
-        '';
-
-        installPhase = ''
-          npm install --no-fund --no-audit
-          npm run build
-          
           mkdir -p $out/usr/share/notion-calendar-electron
           mkdir -p $out/usr/bin
           mkdir -p $out/usr/share/applications
 
-          cp -r dist/linux-unpacked/* $out/usr/share/notion-calendar-electron
+          cp -r ./dist/linux-unpacked/* $out/usr/share/notion-calendar-electron
           cp -P ./notion-calendar-electron $out/usr/bin/
           cp ./icon.png $out/usr/share/notion-calendar-electron/
           cp ./notion-calendar-electron.desktop $out/usr/share/applications/
-        '';
+          '';
 
-        meta = with pkgs.lib; {
-          description = "A Electron-based Notion Calendar client";
-          homepage = "https://www.notion.so/product/calendar";
-          license = licenses.mit;
-          platforms = platforms.linux;
         };
-      };
-
-      # Explicitly set the default package
-      defaultPackage.${system} = pkgs.${system}.default;
-    });
+      in
+        {
+          packages = {
+            node-modules = node-modules;
+            default = package;
+          };
+        }
+    );
 }
--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 #!/usr/bin/env node
 
-const { app, BrowserWindow,ipcMain} = require('electron');
+const { app, BrowserWindow } = require("electron");
+app.commandLine.appendSwitch("disable-gpu");
+app.commandLine.appendArgument("disable-gpu");
+app.whenReady().then(() => {
+  console.log("App is ready");
 
-app.on('ready', () => {
+  // app.on("ready", () => {
+  console.log("App on ready event");
 
-    // Create a new window
-    let win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        autoHideMenuBar: true,
-    });
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+  });
 
-    // Load the external website
-    win.loadURL('https://calendar.notion.so/');
+  // Load the external website
+  win.loadURL("https://calendar.notion.so/");
 
-    //win.webContents.openDevTools();
+  //win.webContents.openDevTools();
 
-    win.webContents.setWindowOpenHandler((details) => {
+  win.webContents.setWindowOpenHandler((details) => {
+    console.log(typeof details.url);
 
-		console.log(typeof details.url);
+    if (details.url.includes("https://calendar.notion.so/google-permissions")) {
+      return {
+        action: "allow",
+      };
+    }
 
-		if(details.url.includes("https://calendar.notion.so/google-permissions")) {
-        		return {
-            			action: 'allow'
-        		};
-		}
-
-        require('electron').shell.openExternal(details.url);
-        return {
-            action: 'deny'
-        };
-    });
-
+    require("electron").shell.openExternal(details.url);
+    return {
+      action: "deny",
+    };
+  });
+  // });
 });
