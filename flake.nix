e617883414135ffc3d00900e291e1cf735db42c1
czlabinger <czlabinger@student.tgm.ac.at>
2024-11-02 00:38:26 +0100

    Fixed overlay

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
           type = "app";
           program = "${self.packages.${system}.default}/bin/notion-calendar-electron";
         };
-        overlay = import ./nix/overlays.nix {inherit self;};
+        overlay = import ./nix/overlays.nix {inherit self;}.overlay;
       }
     );
 }
--- nix/overlays.nix
+++ nix/overlays.nix
@@ -1,6 +1,6 @@
-{ self }:
-[
-  (final: prev: {
+{ self }: {
+  # Export a single overlay instead of a list
+  overlay = final: prev: {
     notion-calendar-electron = self.packages.${prev.system}.default;
-  })
-]
+  };
+}
